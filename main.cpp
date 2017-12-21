7e67ac341860b2304d2483f1c1488c3b958cccb1
wildeKaiserin <friedri5@hm.edu>
2017-12-21 12:50:55 +0100

    try to run travis

--- main.cpp
+++ main.cpp
@@ -5,18 +5,6 @@
 #include <AvlTree.h>
 
 int main() {
-        AvlTree avl;
-        avl.insert(44);
-        avl.insert(17);
-        avl.insert(62);
-        avl.insert(32);
-        avl.insert(50);
-        avl.insert(78);
-        avl.insert(48);
-        avl.insert(54);
-        avl.insert(88);
-        avl.remove(88);
-        cout << avl << endl;
         return 0;
 };
 
